1b934139a5e30ecefca386384a98d240751f3096
Stephanie Vizzi <stephanie@smartlogic.io>
2022-07-11 09:15:17 -0400

    Additions to tests: check specifically for items in resulting files

--- test/csv_parser_test.exs
+++ test/csv_parser_test.exs
@@ -20,18 +20,31 @@ defmodule ClientImporter.CSVParserTest do
      result = CSVParser.parse("test/fixtures/client_importer_valid.csv", "email")
 
       assert result == :ok
+      assert "priv/repo/csv/scrubbed_data/clients_by_unique_email.csv"
+
+      scrubbed_file = File.read!("priv/repo/csv/scrubbed_data/clients_by_unique_email.csv")
+      refute String.contains?(scrubbed_file, "Alfie")
     end
 
     test "success: valid file for phone" do
      result = CSVParser.parse("test/fixtures/client_importer_valid.csv", "phone")
 
       assert result == :ok
+      assert "priv/repo/csv/scrubbed_data/clients_by_unique_phone.csv"
+
+      scrubbed_file = File.read!("priv/repo/csv/scrubbed_data/clients_by_unique_phone.csv")
+      refute String.contains?(scrubbed_file, "Teresa")
     end
 
     test "success: valid file for email or phone" do
      result = CSVParser.parse("test/fixtures/client_importer_valid.csv", "email_or_phone")
 
       assert result == :ok
+      assert "priv/repo/csv/scrubbed_data/clients_by_unique_phone_and_email.csv"
+
+      scrubbed_file = File.read!("priv/repo/csv/scrubbed_data/clients_by_unique_phone_and_email.csv")
+      refute String.contains?(scrubbed_file, "Alfie")
+      refute String.contains?(scrubbed_file, "Teresa")
     end
   end
 end
